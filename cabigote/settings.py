f9f8cb84c172d99bb84ef20848bcfa53901bb8e0
Jose <lanzaltura17@gmail.com>
2024-12-07 12:49:52 +0000

    Eliminando configuraciones SSL por error desarrarollo

--- cabigote/settings.py
+++ cabigote/settings.py
@@ -173,19 +173,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Configuraciones SSL
-
-if not DEBUG:  # Aplica solo si DEBUG está deshabilitado (producción)
-    SECURE_SSL_REDIRECT = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-
-
 
 # Define la ruta absoluta para el archivo de logs
 LOG_FILE_DIR = os.path.join(BASE_DIR, 'logs')
