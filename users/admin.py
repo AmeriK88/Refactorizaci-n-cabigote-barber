551f79af264e753916ee82c3599bc8953d291b4c
Jose <lanzaltura17@gmail.com>
2024-12-09 19:12:39 +0000

    Actualizando orden installed apps & modifico admin.py app users x error configuración

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,3 @@
-from django.apps import AppConfig
+from django.contrib import admin
 
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+# Register your models here.
