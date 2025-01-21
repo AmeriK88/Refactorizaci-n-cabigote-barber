7dcbe19d0bb6f49689daa8d2f8c4389248d0acf7
AmeriK88 <lanzaltura17@gmail.com>
2025-01-21 01:12:59 +0000

    Probando nueva configuracion archivos estaticos

--- cabigote/settings.py
+++ cabigote/settings.py
@@ -164,7 +164,7 @@ LOGIN_REDIRECT_URL = '/users/perfil/'
 LOGOUT_REDIRECT_URL = '/' 
 
 # Archivos subidos por el usuario (Media)
-MEDIA_URL = '/static/'  
+MEDIA_URL = '/media/'  
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Archivos estáticos (CSS, JavaScript, Imágenes)
